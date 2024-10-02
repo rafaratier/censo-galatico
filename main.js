39fb721ef64e94b891d7347b196caec03bf2764a
rafaratier <rafaelratier@hotmail.com>
2024-10-02 11:40:46 -0300

    quarta tarefa

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { createPlanetsButtons, showPlanetData } from "./dom-helpers.js";
 
 const allPlanets = await getAllPlanets();
 
+const searchElem = document.getElementById('search-input');
+
 const btnContainer = document.getElementById('planets-btn-container');
 
 const planetCard = document.getElementById('planet-info-card');
@@ -14,4 +16,10 @@ document.addEventListener('click', ({ target }) => {
     const selectedPlanet = getPlanet(target.id, allPlanets);
     showPlanetData(selectedPlanet, planetCard);
   };
+});
+
+searchElem.addEventListener('input', ({ target }) => {
+  const searchTerm = target.value.toLowerCase();
+  const selectedPlanet = getPlanet(searchTerm, allPlanets);
+  if (selectedPlanet != undefined) showPlanetData(selectedPlanet, planetCard);
 });
